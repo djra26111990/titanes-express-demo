9b493790d15215918ea37a1fcd5e9588501fa39b
Daniel Rivas <djra26111990@gmail.com>
2021-02-01 17:53:01 -0500

    first commit - deploy demo

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const compression = require('compression');
+const cors = require('cors')/*
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminOptipng = require('imagemin-optipng');*/
+
+const PORT = process.env.PORT || 5000;
+
+/*
+(async () => {
+    const files = await imagemin(['public/img/*.{jpg,png}'], {
+        destination: './public/img/build',
+        plugins: [
+            imageminJpegtran(),
+            imageminOptipng()
+        ]
+    });
+ 
+    console.log(files);
+    
+})();*/
+
+
+app.use(compression());
+app.use(cors());
+app.use(express.static('public'));
+
+const listener = app.listen(PORT, '0.0.0.0', function() {
+	console.log(`Web app running at https://0.0.0.0/${PORT}`);
+});
