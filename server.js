1c3ad99c806bb1cef827d7650d2b35f9edc1b2af
Daniel Rivas <djra26111990@gmail.com>
2021-02-01 18:04:06 -0500

    third commit - deploy demo

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(cors());
-app.use(express.static('public/index.html'));
+app.use(express.static('./public'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join__dirname, '/public/index.html'));
+});
+
 
 const listener = app.listen(PORT, '0.0.0.0', function() {
 	console.log(`Web app running at https://0.0.0.0/${PORT}`);
