2a60426b579e2b0bcf39fa09717c36cf841136cc
Daniel Rivas <djra26111990@gmail.com>
2021-02-01 18:11:59 -0500

    fifth commit - deploy demo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const compression = require('compression');
 const cors = require('cors')/*
 const imagemin = require('imagemin');
