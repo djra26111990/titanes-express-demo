d0cead8aa91a0bbf7e1e52f1d08c25b6fabd6259
Daniel Rivas <djra26111990@gmail.com>
2021-02-01 18:34:28 -0500

    push to github

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(cors());
-app.use(express.static('./public'));
+app.use(express.static('./views'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 
